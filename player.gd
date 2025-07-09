fc83e318ebb8b5243716c6c7f97c10b49c28c9a8
Raksakoreko <Nicolas.zimmermann75116@gmail.com>
2025-07-09 02:14:12 +0200

    Crouch patch
    - Retirer le lerp ducoup plus d'animation
    - Mais patch avec l'ajout de 2 hitbox + raycast

--- player.gd
+++ player.gd
@@ -6,15 +6,15 @@ extends CharacterBody3D
 @export var gravity := 12.0
 @export var jump_velocity := 7.0
 
-@export var crouch_height := 1.0  # Hauteur du capsule accroupi
-@export var stand_height := 1.8   # Hauteur du capsule debout
-@export var crouch_camera_y := 0.4 # Y caméra accroupi
-@export var stand_camera_y := 0.8  # Y caméra debout
+@export var crouch_camera_y := 0.4
+@export var stand_camera_y := 0.8
 
 var rotation_x := 0.0
+var is_crouched := false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	set_crouch(false)
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -23,14 +23,11 @@ func _input(event):
 		rotation_x = clamp(rotation_x, deg_to_rad(-90), deg_to_rad(90))
 		$Camera3D.rotation.x = rotation_x
 
-func can_stand_up() -> bool:
-	var collider = $CollisionShape3D.shape
-	var current_height = collider.height
-	var stand_diff = stand_height - current_height
-	if stand_diff < 0.01:
-		return true # Déjà debout
-	var up = Vector3.UP * (stand_diff * 0.5)
-	return not test_move(global_transform, up)
+func set_crouch(state : bool):
+	is_crouched = state
+	$CollisionShape3D_Standing.disabled = state
+	$CollisionShape3D_Crouched.disabled = not state
+	$Camera3D.position.y = crouch_camera_y if state else stand_camera_y
 
 func _physics_process(_delta):
 	var direction = Vector3.ZERO
@@ -45,32 +42,16 @@ func _physics_process(_delta):
 	direction.y = 0
 	direction = direction.normalized()
 
-	# Crouch logic
-	var is_crouching = Input.is_action_pressed("crouch")
-	var current_speed = crouch_speed if is_crouching else speed
-	
-	var wants_to_stand = not Input.is_action_pressed("crouch")
-	var can_stand = can_stand_up()
+	var current_speed = crouch_speed if is_crouched else speed
 
-	var target_height : float
-	var target_cam_y : float
+	# Gestion crouch avec RayCast3D
+	if Input.is_action_pressed("crouch"):
+		set_crouch(true)
+	elif is_crouched:
+		if not $RayCeiling.is_colliding():
+			set_crouch(false)
+		# sinon reste accroupi
 
-	if wants_to_stand and can_stand:
-		# Autorisé à se relever, on lerp vers debout
-		target_height = stand_height
-		target_cam_y = stand_camera_y
-	elif wants_to_stand and not can_stand:
-		# On veut se relever mais plafond : on force accroupi
-		target_height = crouch_height
-		target_cam_y = crouch_camera_y
-	else:
-		# On veut rester accroupi
-		target_height = crouch_height
-		target_cam_y = crouch_camera_y
-	
-	$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, target_height, 0.2)
-	$Camera3D.position.y = lerp($Camera3D.position.y, target_cam_y, 0.2)
-	
 	velocity.x = direction.x * current_speed
 	velocity.z = direction.z * current_speed
 
